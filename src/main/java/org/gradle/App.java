fc7a11d6ba4059991a4df96d2ecc46cf029b8cca
Kevin Galligan <kgalligan@gmail.com>
2015-12-02 20:52:42 -0500

    import

---
+++ src/main/java/org/gradle/App.java
@@ -0,0 +1,14 @@
+package org.gradle;
+
+/**
+ * Created by kgalligan on 12/2/15.
+ */
+public class App
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Some java");
+	}
+}
+
+
