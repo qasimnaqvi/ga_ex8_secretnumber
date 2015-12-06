5f33bbfd5eabcd2cb759ef5acc633a47d44a1dd4
Kevin Galligan <kgalligan@gmail.com>
2015-12-06 17:38:08 -0500

    Ask name

--- src/main/java/org/gradle/App.java
+++ src/main/java/org/gradle/App.java
@@ -1,5 +1,7 @@
 package org.gradle;
 
+import java.util.Scanner;
+
 /**
  * Created by kgalligan on 12/2/15.
  */
@@ -7,17 +9,31 @@ public class App
 {
 	public static void main(String[] args)
 	{
-		// create a first method called cleanFullName that accepts two parameters: firstName, lastName
-		// this method should make all the letters of both those paramters lowercase and then capitalize the first letter of each
-		// this method should return the combination of the first and last name as one string
+		Scanner inp = new Scanner(System.in);
 
-		// ask the user for their first name
+		System.out.print("Enter your first name: ");
+		String firstName = inp.nextLine();
 
-		// ask the user for their last name
+		System.out.print("Enter your last name: ");
+		String lastName = inp.nextLine();
 
-		// run the first method and store the results in a variable called fullName
+		String fullName = cleanFullName(firstName, lastName);
+		System.out.println("Full name: "+ fullName);
+	}
 
-		// print the fullName to the screen
+	static String cleanFullName(String firstName, String lastName)
+	{
+		firstName = capitalize(firstName);
+		lastName = capitalize(lastName);
+
+		return firstName + " " + lastName;
+	}
+
+	private static String capitalize(String firstName)
+	{
+		firstName = firstName.toLowerCase();
+		firstName = firstName.substring(0, 1).toUpperCase() + firstName.substring(1);
+		return firstName;
 	}
 }
 
