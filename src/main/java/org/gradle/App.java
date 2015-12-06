6cba7f7ad9cbd470f4d9ee1f2302eb395f7477d4
Kevin Galligan <kgalligan@gmail.com>
2015-12-06 17:25:49 -0500

    Ask name

--- src/main/java/org/gradle/App.java
+++ src/main/java/org/gradle/App.java
@@ -7,15 +7,17 @@ public class App
 {
 	public static void main(String[] args)
 	{
-		concatStrings();
-	}
+		// create a first method called cleanFullName that accepts two parameters: firstName, lastName
+		// this method should make all the letters of both those paramters lowercase and then capitalize the first letter of each
+		// this method should return the combination of the first and last name as one string
 
-	private static void concatStrings()
-	{
-		String lastBit = "a string";
-		String wholeString = "This is "+ lastBit;
+		// ask the user for their first name
 
-		System.out.println(wholeString);
+		// ask the user for their last name
+
+		// run the first method and store the results in a variable called fullName
+
+		// print the fullName to the screen
 	}
 }
 
