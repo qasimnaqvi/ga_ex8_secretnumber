e53e4ca1211f9b748b627959b44d98e7b77d885f
Kevin Galligan <kgalligan@gmail.com>
2015-12-06 23:48:07 -0500

    Secret number

--- src/main/java/org/gradle/App.java
+++ src/main/java/org/gradle/App.java
@@ -9,41 +9,49 @@ public class App
 {
 	public static void main(String[] args)
 	{
-		Scanner inp = new Scanner(System.in);
-		System.out.println("did you pay your bills? (yes/no)");
-		String paid = inp.nextLine();
-
-		System.out.println("did you cancel");
-		String canceled = inp.nextLine();
-
-		System.out.println("are you an admin");
-		String admin = inp.nextLine();
-
-		System.out.println("are you signed in");
-		String signedIn = inp.nextLine();
+		System.out.println("Welcome to SECRET NUMBAH! This game has been created by _____________  © 2015. Please enter your first name.");
+		//take the player's name and save it to a variable called player_name
 
 
-		if (paid.equals("no") || canceled.equals("yes"))
-		{
-			System.out.println("go away");
-		}
-		else if (signedIn.equals("yes") && admin.equals("yes"))
-		{
-			System.out.println("you can see and change all the pages");
-		}
-		else if (signedIn.equals("yes") && admin.equals("no"))
-		{
-			System.out.println("you can see all the pages");
-		}
-		else if (signedIn.equals("no"))
-		{
-			System.out.println("you can't see any of the pages, please sign in");
-		}
-		else
-		{
-			System.out.println("sorry charlie");
-		}
+		System.out.println("Hello there, #{player_name}.  Here are the rules:");
+		System.out.println("1. You will try to guess a number between 1 and 10");
+		System.out.println("2. You have three tries");
+
+		//run the function to check the user's number
+
+		//bonus: add one more line that says "you didn't guess it was INSERT_THE_NUMBER.  better luck next time" if the @did_they_win variable isn't true
+
 	}
+
+
+	static void check_users_number()
+	{
+	// define the number of tries the user has and save it to a variable
+	// google how to randomly generate a number between 1-10, save that number to a variable called @random_number
+		System.out.println("Your SECRET NUMBAH has been chosen - guess a numbah between 1 and 10!");
+
+		// add a line that loops through this until the player has no tries left
+		Scanner inp = new Scanner(System.in);
+		int player_guess = Integer.parseInt(inp.nextLine());
+
+	//check to see if the player_guess is equal to the @random_number
+		System.out.println("OH MY GAWD YOU GUESSED THE SECRET NUMBAH, #{@random_number}!  YOU WIN!");
+		//set a variable called @did_they_win and set it equal to true
+		break
+		//check to see if the player has more tries left
+	tries_left -=1;
+		//check to see if the player_guess is more than the @random_number
+		if tries_left > 0
+		System.out.println("You guessed too high, silly! You have #{tries_left} guesses before the game is over enter a another number");
+		else
+		if tries_left > 0
+		System.out.println("You guessed too low, silly! You have #{tries_left} guesses before the game is over enter a another number"
+	}
+
+
+
+
+
 }
 
 
