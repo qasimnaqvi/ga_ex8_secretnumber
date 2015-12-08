f05ead6d61f1659a8229b432ba9f3aa8c6936665
Kevin Galligan <kgalligan@gmail.com>
2015-12-08 14:33:11 -0500

    Clean up

--- src/main/java/org/gradle/App.java
+++ src/main/java/org/gradle/App.java
@@ -24,28 +24,27 @@ public class App
 	}
 
 
-	static void check_users_number()
+	static boolean check_users_number()
 	{
+		Scanner inp = new Scanner(System.in);
+
 	// define the number of tries the user has and save it to a variable
 	// google how to randomly generate a number between 1-10, save that number to a variable called @random_number
 		System.out.println("Your SECRET NUMBAH has been chosen - guess a numbah between 1 and 10!");
 
 		// add a line that loops through this until the player has no tries left
-		Scanner inp = new Scanner(System.in);
-		int player_guess = Integer.parseInt(inp.nextLine());
+		int playerGuess = Integer.parseInt(inp.nextLine());
 
-	//check to see if the player_guess is equal to the @random_number
+		//check to see if the playerGuess is equal to the randomNumber
 		System.out.println("OH MY GAWD YOU GUESSED THE SECRET NUMBAH, #{@random_number}!  YOU WIN!");
-		//set a variable called @did_they_win and set it equal to true
-		break
-		//check to see if the player has more tries left
-	tries_left -=1;
+		//return true
+
 		//check to see if the player_guess is more than the @random_number
-		if tries_left > 0
 		System.out.println("You guessed too high, silly! You have #{tries_left} guesses before the game is over enter a another number");
-		else
-		if tries_left > 0
+		//else
 		System.out.println("You guessed too low, silly! You have #{tries_left} guesses before the game is over enter a another number"
+
+				//At end of loop, return false
 	}
 
 
