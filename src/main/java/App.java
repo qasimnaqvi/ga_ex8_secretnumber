2ec5d8a6612ef8a1c186218c94493a174b9d5e74
QasimNaqvi <qasimnaqvi0104@gmail.com>
2016-01-03 12:26:43 -0500

    new updates from Q

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,19 +1,28 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class App
 {
 	public static void main(String[] args)
 	{
+		Scanner input = new Scanner( System.in );
+
 		System.out.println("Welcome to SECRET NUMBER! Please enter your first name.");
+		String player_name = input.next();
+
 		//take the player's name and save it to a variable called player_name
 
 
 		//Insert the player's name below
-		System.out.println("Hello there, PLAYER_NAME.  Here are the rules:");
+		System.out.println("Hello there," + player_name + ".  Here are the rules:");
 		System.out.println("1. You will try to guess a number between 0 and 10");
 		System.out.println("2. You have three tries");
 
 		//run the function to check the user's number
+
+		checkUsersNumber();
+
+
 		//If the user guessed correctly, print a congratulations message, otherwise print a failure message
 
 	}
@@ -25,16 +34,40 @@ public class App
 		int secretNumber = r.nextInt(11);
 
 		//Create a loop to get the player's guess and check it
+		//Get the user's guess
 
 		System.out.println("Your SECRET NUMBER has been chosen - guess a number between 0 and 10!");
-		//Get the user's guess
+		System.out.println(secretNumber);
+		Scanner input = new Scanner( System.in );
+
+		int inputNumber = input.nextInt();
+		boolean isCorrect = false;
+		int counter = 1;
+
+		if(inputNumber == secretNumber){
+			System.out.println("Nice. You got speciall skills!");
+
+		while(inputNumber != secretNumber && counter<3) {
+			//Check the guess
+
+			if(inputNumber>secretNumber) {
+				System.out.println("You guessed too high, silly! You have " + (3-counter) + " guesses before the game is over");
+			}
+			if(inputNumber<secretNumber){
+				System.out.println("You guessed too low, silly! You have" + (3-counter) + " guesses before the game is over");
+			}
+			System.out.println("Guess again!");
+			counter++;
+
+			}
+
+			inputNumber = input.nextInt();
+		}
+
 
 		//Check the guess
 
-		//check to see if the player_guess is more than the @random_number
-		System.out.println("You guessed too high, silly! You have REMAINING_GUESSES guesses before the game is over");
-		//else
-		System.out.println("You guessed too low, silly! You have REMAINING_GUESSES guesses before the game is over");
+		return inputNumber == secretNumber;
 
 	}
 }
